3e85e314a71fa2e85aacdb25d3e2ebd8cdcbc744
Shaun Kent <shaunkent81@gmail.com>
2021-08-30 19:15:04 -0700

    initial commit for continuity

---
+++ Palindromes.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+public class Palidrome 
+{
+  
+
+}
+
+
+
+/*
+
+let palindrome = 1010;
+let isString = false;
+if (typeof(palindrome) === 'string') {
+	isString = true;
+}
+
+palindrome = palindrome.toString().split("");
+
+let forwardIndex = 0;
+let backwardIndex = palindrome.length - 1;
+let truePalindrome = true;
+
+while(forwardIndex < palindrome.length) {
+	if (isString) { 
+  	if(palindrome[forwardIndex].toUpperCase() !== palindrome[backwardIndex].toUpperCase()) {
+  	truePalindrome = false;
+    break;
+    }
+  } else if(palindrome[forwardIndex] !== palindrome[backwardIndex]){
+  	truePalindrome = false;
+    break;
+  }
+
+  forwardIndex += 1;
+  backwardIndex -= 1;
+}
+
+console.log(truePalindrome);
+*/
